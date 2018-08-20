1ad2baf53a6ec793e59f08f6e6436c4ac0172843
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2018-08-20 11:50:53 +0700

    Move `ComponentValueProvider` to `component/component-context` module

--- src/component/component-context.ts
+++ src/component/component-context.ts
@@ -73,3 +73,18 @@ export class ComponentValueKey<V> {
   }
 
 }
+
+/**
+ * Component value provider.
+ *
+ * It is responsible for constructing the values associated with particular key for each component.
+ *
+ * This function is called at most once per component, unless it returns `null`/`undefined`. In the latter case
+ * it may be called again later.
+ *
+ * @param <V> The type of associated value.
+ * @param context Target component context.
+ *
+ * @return Either constructed value, or `null`/`undefined` if the value can not be constructed.
+ */
+export type ComponentValueProvider<V> = <E extends HTMLElement>(context: ComponentContext<E>) => V | null | undefined;
--- src/component/index.ts
+++ src/component/index.ts
@@ -2,4 +2,3 @@ export * from './component-class';
 export * from './component-context';
 export * from './component-def';
 export * from './component-type';
-export * from './component-value-provider';
