c0d6a8143e092a64b50efb59dba948de48d87771
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-01-11 15:41:44 +0700

    Introduce `ComponentEvent`

--- src/component/index.ts
+++ src/component/index.ts
@@ -2,4 +2,5 @@ export * from './component.decorator';
 export * from './component-class';
 export * from './component-context';
 export * from './component-def';
+export * from './component-event';
 export * from './component-mount';
