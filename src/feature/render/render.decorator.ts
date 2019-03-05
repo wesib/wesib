f8da32649611d8a8b02765c22103f649b3d31698
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-03-05 19:47:48 +0700

    Better document rendering conditions

--- src/feature/render/render.decorator.ts
+++ src/feature/render/render.decorator.ts
@@ -76,7 +76,7 @@ export namespace Render {
      * When offline rendering is disabled the rendering will be scheduled whenever element is connected.
      *
      * `false` by default. Which means the element contents won't be rendered while disconnected. Rendering will
-     * be initiated once element is connected.
+     * be initiated once element is connected for the first time, or if it is connected after state update.
      */
     offline?: boolean;
 
