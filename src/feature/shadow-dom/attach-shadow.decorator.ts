6f214393e414844b64fa2ee4f15450dc7d1e5d3a
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-04-25 13:44:18 +0700

    Add `ShadowContentDef.componentDef()` function
    Define shadow content root using this function.
    Do not eagerly attach shadow content root to the component.

--- src/feature/shadow-dom/attach-shadow.decorator.ts
+++ src/feature/shadow-dom/attach-shadow.decorator.ts
@@ -1,50 +1,22 @@
 import { TypedClassDecorator } from '../../common';
-import { ComponentClass, ComponentContext, ComponentDef, ContentRoot } from '../../component';
-import { DefinitionContext } from '../../component/definition';
-import { FeatureDef } from '../feature-def';
+import { ComponentClass, ComponentDef } from '../../component';
 import { ShadowContentDef } from './shadow-content-def';
-import { ShadowContentRoot } from './shadow-content-root';
-import { ShadowDomSupport } from './shadow-dom-support.feature';
-import { ShadowRootBuilder } from './shadow-root-builder';
-
-const defaultShadowContentDef: ShadowContentDef = { mode: 'open' };
 
 /**
  * Component class decorator that attaches shadow root to decorated component instance.
  *
+ * Applies component definition created by `ShadowContentDef.componentDef()` function.
+ *
  * @typeparam T A type of component.
- * @param init Shadow root initialization options. Uses `mode: 'open'` by default.
+ * @param def Shadow content root definition. Uses `mode: 'open'` by default.
  *
  * @return Component class decorator.
  */
 export function AttachShadow<T extends ComponentClass<any> = any>(
-    init: ShadowContentDef = defaultShadowContentDef): TypedClassDecorator<T> {
+    def?: ShadowContentDef): TypedClassDecorator<T> {
   return (type: T) => {
-    FeatureDef.define(type, { needs: [ShadowDomSupport] });
     ComponentDef.define(
         type,
-        {
-          define(this: ComponentClass<InstanceType<T>>, context: DefinitionContext<InstanceType<T>>) {
-            // Attach shadow root eagerly when component is ready.
-            context.onComponent(ctx =>
-                ctx.whenReady(() =>
-                    ctx.get(ShadowContentRoot)));
-          },
-          perComponent: [
-            {
-              a: ShadowContentRoot,
-              by(ctx: ComponentContext<InstanceType<T>>) {
-                return ctx.get(ShadowRootBuilder)(ctx, init);
-              },
-            },
-            { // Content root is an alias of shadow root.
-              a: ContentRoot,
-              by(shadowRoot: ShadowContentRoot) {
-                return shadowRoot;
-              },
-              with: [ShadowContentRoot],
-            },
-          ],
-        });
+        ShadowContentDef.componentDef(def));
   };
 }
