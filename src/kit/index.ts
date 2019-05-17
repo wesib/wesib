cd0c2ac7981c49ba360223bfb90a1df142f39437
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-05-17 16:34:43 +0700

    `bootstrapComponents()`: Provide default namespace aliaser

--- src/kit/index.ts
+++ src/kit/index.ts
@@ -2,4 +2,5 @@ export * from './bootstrap-context';
 export * from './bootstrap-root';
 export * from './bootstrap-window';
 export * from './component-kit';
+export * from './default-namespace-aliaser';
 export * from './element-adapter';
