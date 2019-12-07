e61db0ab82fc31471a4970add876a93b0afc8a3d
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-12-07 10:18:13 +0700

    Bootstrap test

---
+++ src/boot/bootstrap/bootstrap.spec.ts
@@ -0,0 +1,63 @@
+import { SingleContextUpKey } from 'context-values';
+import { eventSupply, EventSupply } from 'fun-events';
+import { Class } from '../../common';
+import { Feature } from '../../feature';
+import { BootstrapContext } from '../bootstrap-context';
+import { bootstrapComponents } from './bootstrap-components';
+import Mock = jest.Mock;
+
+describe('boot', () => {
+
+  let bsContext: BootstrapContext;
+
+  beforeEach(async () => {
+    bsContext = bootstrapComponents();
+    await new Promise(resolve => bsContext.whenReady(resolve));
+  });
+
+  describe('context values', () => {
+
+    let key: SingleContextUpKey<string>;
+    let receiver: Mock<void, [string]>;
+
+    beforeEach(() => {
+      key = new SingleContextUpKey<string>('test-key', { byDefault: () => 'default' });
+      receiver = jest.fn();
+    });
+
+    it('sets up bootstrap context values', async () => {
+      bsContext.get(key)(receiver);
+
+      @Feature({
+        set: { a: key, is: 'provided' },
+      })
+      class TestFeature {}
+
+      const supply = await loadFeature(TestFeature);
+
+      expect(receiver).toHaveBeenLastCalledWith('provided');
+
+      supply.off();
+      await Promise.resolve();
+      expect(receiver).toHaveBeenLastCalledWith('default');
+    });
+  });
+
+  function loadFeature(
+      feature: Class,
+  ): Promise<EventSupply> {
+    return new Promise(resolve => {
+
+      const supply = eventSupply();
+
+      bsContext.load(feature)({
+        supply,
+        receive(_ctx, { ready }) {
+          if (ready) {
+            resolve(supply);
+          }
+        },
+      });
+    });
+  }
+});
