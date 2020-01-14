281149e6e11e88ba1815e1d63a35ebe60f44c856
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-01-14 14:35:44 +0700

    Make `DefaultRenderScheduler` updatable

--- src/boot/globals/default-render-scheduler.ts
+++ src/boot/globals/default-render-scheduler.ts
@@ -1,4 +1,4 @@
-import { ContextRef, SingleContextKey } from 'context-values';
+import { FnContextKey, FnContextRef } from 'context-values';
 import { newRenderSchedule, RenderScheduler } from 'render-scheduler';
 import { bootstrapDefault } from '../bootstrap-default';
 import { BootstrapWindow } from './bootstrap-window';
@@ -15,8 +15,9 @@ export type DefaultRenderScheduler = RenderScheduler;
  *
  * Uses the default `RenderScheduler` (`newRenderSchedule()`) for {@link BootstrapWindow bootstrap window}.
  */
-export const DefaultRenderScheduler: ContextRef<DefaultRenderScheduler> =
-    (/*#__PURE__*/ new SingleContextKey<DefaultRenderScheduler>(
+export const DefaultRenderScheduler:
+    FnContextRef<Parameters<DefaultRenderScheduler>, ReturnType<DefaultRenderScheduler>> =
+    (/*#__PURE__*/ new FnContextKey<Parameters<DefaultRenderScheduler>, ReturnType<DefaultRenderScheduler>>(
         'default-render-scheduler',
         {
           byDefault: bootstrapDefault(
