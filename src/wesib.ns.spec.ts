63cb6b812658a848a1de37ec20dd60bcd68aa74e
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-01-27 15:18:21 +0700

    Default Wesib namespace

---
+++ src/wesib.ns.spec.ts
@@ -0,0 +1,10 @@
+import { Wesib__NS } from './wesib.ns';
+
+describe('Wesib__NS', () => {
+  it('has Wesib URL', () => {
+    expect(Wesib__NS.url).toBe('https://wesib.github.io/ns');
+  });
+  it('has `b` namespace alias', () => {
+    expect(Wesib__NS.alias).toBe('b');
+  });
+});
