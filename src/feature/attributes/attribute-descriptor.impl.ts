5ceea41fdd6976fc3c68bca821f02c1fd60f87d1
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-02-06 16:49:06 +0700

    Utilize `hyphenateDecapName()` for attribute name conversion

--- src/feature/attributes/attribute-descriptor.impl.ts
+++ src/feature/attributes/attribute-descriptor.impl.ts
@@ -1,7 +1,7 @@
+import { hyphenateDecapName } from '@frontmeans/httongue';
 import { AttributeDef } from './attribute-def';
 import { AttributeChangedCallback, AttributeDescriptor } from './attribute-descriptor';
 import { attributeStateUpdate } from './attribute-state-update.impl';
-import { property2attributeName } from './property2attribute-name';
 
 /**
  * @internal
@@ -9,28 +9,28 @@ import { property2attributeName } from './property2attribute-name';
 export function parseAttributeDescriptor<T extends object>(
     target: T,
     propertyKey: string | symbol,
-    opts?: AttributeDef<T> | string,
+    def?: AttributeDef<T> | string,
 ): AttributeDescriptor<T> {
 
   let name: string;
   let change: AttributeChangedCallback<T>;
 
-  if (typeof opts === 'string') {
-    name = property2attributeName(opts);
+  if (typeof def === 'string') {
+    name = hyphenateDecapName(def);
     change = attributeStateUpdate(name);
   } else {
-    if (opts && opts.name) {
-      name = property2attributeName(opts.name);
+    if (def && def.name) {
+      name = hyphenateDecapName(def.name);
     } else if (typeof propertyKey !== 'string') {
       throw new TypeError(
           'Attribute name is required as property key is not a string: '
           + `${target.constructor.name}.prototype.${String(propertyKey)}`,
       );
     } else {
-      name = property2attributeName(propertyKey);
+      name = hyphenateDecapName(propertyKey);
     }
 
-    change = attributeStateUpdate(name, opts && opts.updateState);
+    change = attributeStateUpdate(name, def && def.updateState);
   }
 
   return { name, change };
