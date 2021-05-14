dd7d6fad1722ad52cad194e9fb9498ce14681bf0
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-05-14 16:06:51 +0700

    Convert `@StateProperty()` to amendment

--- src/feature/state/index.ts
+++ src/feature/state/index.ts
@@ -1,4 +1,4 @@
 export * from './component-state';
-export * from './state-property.decorator';
+export * from './state-property.amendment';
 export * from './state-property-path';
 export * from './track-state-property';
